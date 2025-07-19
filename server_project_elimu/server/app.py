173dc81e06561dea3820861a956a4c59cbdc0c08
stlmargy <stellahmargy0211@gmail.com>
2025-07-19 17:14:04 +0300

    installs flask , creates app instance and sets conf for it run

--- server_project_elimu/server/app.py
+++ server_project_elimu/server/app.py
@@ -1,1 +1,9 @@
-app="my flask application instance "
+from flask import Flask
+
+#Instantiate a flask app instance
+app=Flask(__name__)
+
+# Test the application by running it and navigating to a particular route
+@app.route('/')
+def home():
+    return "Flask application loading"
