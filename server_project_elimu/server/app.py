ec5bc88287c5306c58220b844dc5af2edd91707d
stlmargy <stellahmargy0211@gmail.com>
2025-07-19 22:33:54 +0300

    implements database configuration

--- server_project_elimu/server/app.py
+++ server_project_elimu/server/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from server.models import db
+from .config import Config
 
 #Instantiate a flask app instance
 app=Flask(__name__)
-
+#load configurations
+app.config.from_object(Config)
 #initialize the app with the extension
 db.init_app(app)
 
