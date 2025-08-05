a69f5c7680b25e5e8dacc8de3898804a5932a7c4
stlmargy <stellahmargy0211@gmail.com>
2025-08-05 20:48:08 +0300

    short cut for login schema access

--- server_project_elimu/server/schemas/__init__.py
+++ server_project_elimu/server/schemas/__init__.py
@@ -3,3 +3,4 @@ from .instructor_schema import InstructorSchema
 from .parent_schema import ParentSchema
 from .student_schema import StudentSchema
 from .adminstrator_schema import AdministratorSchema
+from .login_schema import LoginSchema
